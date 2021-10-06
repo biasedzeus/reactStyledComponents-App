38f37e401e512345267e4ca8f97461f95e13ddb6
biasedzeus <bhanu.singh1078@gmail.com>
2021-10-06 20:55:06 +0530

    globalStyle added
    global style added with Google Fonts Lobster

--- styled-components-app/src/Components/App.js
+++ styled-components-app/src/Components/App.js
@@ -1,9 +1,28 @@
 import Button from "./common/Button";
+import {createGlobalStyle} from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+body{
+  /* background:white; */
+  min-height: 100vh;
+  margin: 0;
+  color:white;
+  font-family: 'Lobster' , 'sans-seriff';
+
+
+}
+
+
+
+
+
+`
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <GlobalStyle/>
+      <h1>Web Design</h1>
       <Button>Log IN</Button>
     </div>
   );
